da110f732302f995c224a325afe1131ffb910b86
cmy <1732728869@qq.com>
2020-03-09 22:23:12 +0800

    项目初始化

--- src/main.js
+++ src/main.js
@@ -2,6 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 导入vant
+import Vant from 'vant'
+import 'vant/lib/index.css'
+// 注册vant
+Vue.use(Vant)
 
 Vue.config.productionTip = false
 
