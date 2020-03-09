9a1f3755da88dba99a5186a53b6f8bf5c4ba63e1
cmy <1732728869@qq.com>
2020-03-09 22:39:16 +0800

    完成移动端适配

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 导入rem适配
+import 'amfe-flexible'
 // 导入vant
 import Vant from 'vant'
 import 'vant/lib/index.css'
